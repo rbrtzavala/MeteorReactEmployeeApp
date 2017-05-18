ca9f53903a3cdd58f62589739ba8faedbc5d2fd2
RAZ <rbrtzavala@gmail.com>
2017-05-18 12:19:40 -0600

    Refactored EmployeeList to class based componenet. Setup button with an event handler to update subscription with additional number of employees.

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { Component} from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
 import EmployeeDetail from './employee_detail';
 
-const PER_PAGE = 20;
+const PER_PAGE = 21;
 
-const EmployeeList = (props) => {
-  // props.employees => an array of employee objects
+class EmployeeList extends Component {
+  componentWillMount() {
+    this.page = 1;
+  }
 
+  handleButtonClick() {
+    Meteor.subscribe('employees', PER_PAGE * (this.page + 1));
+    this.page += 1;
+  }
 
-  return (
-    <div>
-      <div className="employee-list">
-        {props.employees.map(employee =>
-          <EmployeeDetail employee={employee} key={employee._id}/>
-        )}
+  render() {
+    // props.employees => an array of employee objects
+    return (
+      <div>
+        <div className="employee-list">
+          {this.props.employees.map(employee =>
+            <EmployeeDetail key={employee._id} employee={employee} />
+          )}
+        </div>
+        <button onClick={this.handleButtonClick.bind(this)}
+         className="btn btn-primary">
+         Load More...
+        </button>
       </div>
-      <button onClick={() => Meteor.subscribe('employees', 40)}
-       className="btn btn-primary">
-       Load More...
-      </button>
-    </div>
-  )
+    );
+  }
 };
 
 // createConainer() here.
